1ef3c80eba6fb37b2b7d1351315b47580b03e6ce
Ingrid G <ing.gape18@gmail.com>
2024-06-24 21:58:46 -0500

    using useRoute to get id

--- src/views/DetailView.vue
+++ src/views/DetailView.vue
@@ -1,13 +1,30 @@
 <script setup>
-import HeaderComponent from '@/components/HeaderComponent.vue'
+import { onMounted, ref } from 'vue'
+import { useRoute } from 'vue-router'
 import { apiData, fetchData } from '../api'
+
+import HeaderComponent from '@/components/HeaderComponent.vue'
+
+// using route info to fetch id data //route.params, route.name, route.path, route.fullPath,
+const route = useRoute()
+const data = ref([])
+
+const getData = async () => {
+    await fetchData('series/'+`${route.params.id}`);
+    data.value = apiData.value;
+    console.log(data.value)
+};
+  
+onMounted(() => {
+    getData();
+});
 
 </script>
 
 <template>
     <HeaderComponent></HeaderComponent>
     <div class="about">
-        <h1>This is a detail page</h1>
+        <h1>This is a detail page {{ route.params.id }}</h1>
     </div>
 </template>
 
